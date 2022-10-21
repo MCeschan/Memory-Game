ea1b34473209cd834aa31b5c3cc98bc2efc16229
Martina Ceschan <martinaceschan@gmail.com>
2022-10-21 13:38:34 -0300

    style changes/logic alert

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import './App.css'
 import { useEffect, useState } from 'react'
 import SingleCard from "./components/SingleCard"
+import Swal from 'sweetalert2'
+
+
 
 
 const cardImages = [
@@ -39,12 +42,41 @@ function App() {
     if(card.id === choiceOne?.id) return;
     choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
   }
-
+  
+  const handleCheck = (cards) =>{
+    if(cards.filter((card) => card.matched === false).length === 2){
+      Swal.fire({
+        title: 'We have a winner!',
+        width: 600,
+        confirmButtonText: 'New Game',
+        padding: '3em',
+        color: '#F0D844',
+        imageUrl: 'https://i.gifer.com/6uE2.gif',
+        imageWidth: 400,
+        imageHeight: 250,
+        imageAlt: 'Custom image',
+        allowOutsideClick: false,
+        background: '#1b1523',
+        backdrop: `
+          rgba(156,144,61,0.6)
+          url("https://i.gifer.com/6mb.gif")
+          left bottom
+          no-repeat
+        `
+      })
+      shuffleCards()
+      setDisabled(false)
+    } else{
+      resetTurn()
+    }
+  }
+  
 //compare 2 selected cards
   useEffect(() =>{
     if (choiceOne && choiceTwo){
       setDisabled(true)
       if(choiceOne.src === choiceTwo.src){
+        
         setCards(prevCards =>{
           return prevCards.map(card =>{
             if (card.src === choiceOne.src){
@@ -53,15 +85,19 @@ function App() {
               return card
             }
           })
+          
         })
-        resetTurn()
+        handleCheck(cards)
+       
       } else{
         
         setTimeout(() => resetTurn(), 1000)
       }
     }
+    
 
   }, [choiceOne, choiceTwo])
+
 
   
 // reset choices and increase turn
@@ -70,6 +106,28 @@ function App() {
     setChoiceTwo(null)
     setTurns(prevTurns => prevTurns + 1)
     setDisabled(false)
+    if(turns === 11){
+      Swal.fire({
+        title: 'Well...',
+        width: 600,
+        confirmButtonText: 'New Game',
+        padding: '3em',
+        color: '#F0D844',
+        imageUrl: 'https://i.gifer.com/1Q1J.gif',
+        imageWidth: 400,
+        imageHeight: 250,
+        imageAlt: 'Custom image',
+        allowOutsideClick: false,
+        background: '#1b1523',
+        backdrop: `
+          rgba(156,144,61,0.6)
+          url("https://i.gifer.com/7VE.gif")
+          left bottom
+          no-repeat
+        `
+      })
+      shuffleCards()
+     }
   } 
 //start a new game automatically
   useEffect(() => {
@@ -77,11 +135,19 @@ function App() {
   }, [])
 
 
-
+  
   return (
     <div className="App">
       <h1>Memory Game</h1>
       <button onClick={shuffleCards}>New Game</button>
+      <div className="instructions">
+        <p>You have only <br/>   12 turns <br/> to win.<br/>
+        Be careful!</p>
+      </div>
+      <div className="turn">
+      <p>Turns: {turns}</p>
+      </div>
+      
       <div className="card-grid">
         {cards.map(card =>(
           <SingleCard 
@@ -92,8 +158,9 @@ function App() {
           disabled={disabled}
           />
         ))}
+        
       </div>
-      <p>Turns: {turns}</p>
+      
     </div>
   );
 }
